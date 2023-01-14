aaf2758f8bfb3c5fa73e9c58df995014e7d16135
Abanoub Nasser <abanoubnasser@Mariams-MacBook-Pro.local>
2023-01-14 16:40:48 +0400

    Merge time slot matcher for fees

--- src/main/java/com/evolve/service/tollcalculator/service/TimeSlotFeesMatcherService.java
+++ src/main/java/com/evolve/service/tollcalculator/service/TimeSlotFeesMatcherService.java
@@ -14,19 +14,10 @@ import java.util.function.Predicate;
 public class TimeSlotFeesMatcherService {
 
     public int getFeesByDate(LocalDateTime date) {
-
         int hour = date.getHour();
         int minute = date.getMinute();
 
-        Predicate<TollTimeSlotFee> tollTimeSlotFeePredicateForSpecificHour = slot -> (slot.getFromHour() == hour && minute >= slot.getFromMinute() && minute <= slot.getToMinute());
-
-        Optional<TollTimeSlotFee> optionalTollTimeSlotFeeForSpecificHour = findTollTimeSlotFee(tollTimeSlotFeePredicateForSpecificHour);
-        if (optionalTollTimeSlotFeeForSpecificHour.isPresent()) {
-            return optionalTollTimeSlotFeeForSpecificHour.get().getFees();
-        }
-
-        Predicate<TollTimeSlotFee> tollTimeSlotFeePredicateForSpecificRange = slotForRange -> (hour >= slotForRange.getFromHour() && hour < slotForRange.getToHour() && minute >= slotForRange.getFromMinute() && minute <= slotForRange.getToMinute());
-
+        Predicate<TollTimeSlotFee> tollTimeSlotFeePredicateForSpecificRange = slotForRange -> (hour >= slotForRange.getFromHour() && hour <= slotForRange.getToHour() && minute >= slotForRange.getFromMinute() && minute <= slotForRange.getToMinute());
         Optional<TollTimeSlotFee> optionalTollTimeSlotFeeForSpecificRange = findTollTimeSlotFee(tollTimeSlotFeePredicateForSpecificRange);
         if (optionalTollTimeSlotFeeForSpecificRange.isPresent()) {
             return optionalTollTimeSlotFeeForSpecificRange.get().getFees();
